9b63ad60ae4235107d647d71809deb0156179907
pro16 <matteo.porrani@gmail.com>
2022-10-13 01:45:12 +0200

    Changement texte home

--- src/views/PageHome.vue
+++ src/views/PageHome.vue
@@ -9,14 +9,14 @@
         <div class="col-10 col-md-8 order-1 order-md-0 text-center text-md-start">
           <h1 class="res-fs-16 res-fs-md-18">Matteo Porrani</h1>
           <h2 class="text-white fw-semibold">Développeur Web</h2>
-          <p class="res-fs-5 res-fs-md-6 mt-5 pe-md-5">
-            Après une formation dev web fullstack à <b>La Passerelle</b>,
-            <br>j’ai commencé à expérimenter avec les différents langages et frameworks à travers des projets
+          <p class="res-fs-5 res-fs-md-7 mt-5 pe-md-5">
+            Après une formation bootcamp à <b>La Passerelle</b>,
+            <br>j’ai commencé à expérimenter avec les différents langages et frameworks<br> à travers des projets
             personnels.
             <br>
             <br>
             En mai 2022 j’ai intégré l’équipe de la billetterie en ligne <b>Ticketac.com</b>,
-            <br>où je continue à me perfectionner en Symfony & VueJS.</p>
+            <br>où je continue ma montée en compétences avec Symfony & VueJS.</p>
         </div>
 
         <div
